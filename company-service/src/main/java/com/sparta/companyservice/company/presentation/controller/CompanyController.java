1beec994ad2363fed3ef074c8ae070088f7d799b
rodongwan <rodongwan@burritowallet.com>
2026-05-19 20:44:19 +0900

    style: Controller 코드 포맷 정리

--- company-service/src/main/java/com/sparta/companyservice/company/presentation/controller/CompanyController.java
+++ company-service/src/main/java/com/sparta/companyservice/company/presentation/controller/CompanyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/companies")
+@RequestMapping("/api/v1/companies")
 @RequiredArgsConstructor
 public class CompanyController {
 
