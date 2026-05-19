bf760d5930c463570d1db1534ce15149894b10f1
mindy <92miindy@gmail.com>
2026-05-19 15:03:54 +0900

    chore: gradle 의존성(openfeignm postgreql) 추가, DB 설정 추가

--- order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
@@ -2,8 +2,9 @@ package com.sparta.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableFeignClients
+@EnableFeignClients
 @SpringBootApplication
 public class OrderServiceApplication {
 
