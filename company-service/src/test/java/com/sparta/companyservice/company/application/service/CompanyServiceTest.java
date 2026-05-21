0fa0717259a457d997a7d299d9006b20da968485
rodongwan <rodongwan@burritowallet.com>
2026-05-21 19:48:07 +0900

    style: 테스트 코드 내 Mockito 와일드카드 임포트 제거

--- company-service/src/test/java/com/sparta/companyservice/company/application/service/CompanyServiceTest.java
+++ company-service/src/test/java/com/sparta/companyservice/company/application/service/CompanyServiceTest.java
@@ -29,7 +29,13 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CompanyServiceTest {
