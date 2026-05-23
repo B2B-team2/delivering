9fd605b817ddd9d7347f8ae920637340d0f32fcc
rlaalsdn0421 <rlaalsdn0421@naver.com>
2026-05-24 02:22:31 +0900

    feat : [KMW] 배송 서비스 레포지 토리

--- delivery-service/src/main/java/com/sparta/deliveryservice/delivery/infrastructure/persistence/DeliveryJpaRepository.java
+++ delivery-service/src/main/java/com/sparta/deliveryservice/delivery/infrastructure/persistence/DeliveryJpaRepository.java
@@ -5,8 +5,11 @@ import com.sparta.deliveryservice.delivery.domain.core.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DeliveryJpaRepository extends JpaRepository<Delivery, UUID> {
+    Optional<Delivery> findByAddressId(UUID id);
+    Optional<Delivery> findByTrackingNumber(String trackingNumber);
 }
