37776ae30db2527149a30f6b9afe109771e6be68
rlaalsdn0421 <rlaalsdn0421@naver.com>
2026-05-24 02:23:14 +0900

    fix : [KMW] 배송 관리자 변경 요청

--- delivery-service/src/main/java/com/sparta/deliveryservice/delivery/presentation/dto/request/DeliveryManagerUpdateRequest.java
+++ delivery-service/src/main/java/com/sparta/deliveryservice/delivery/presentation/dto/request/DeliveryManagerUpdateRequest.java
@@ -19,6 +19,12 @@ public class DeliveryManagerUpdateRequest {
     @NotNull(message = "변경할 배송 담당자 ID는 필수입니다.")
     private UUID deliveryManagerId;
 
+    @NotBlank(message = "담당자 이름은 필수 입력값입니다.")
+    private String name;
+
+    @NotBlank(message = "담당자 전화번호는 필수 입력값입니다.")
+    private String phone;
+
     @NotBlank(message = "담당자 변경 사유는 필수 입력값입니다.")
     private String reason;
 }
