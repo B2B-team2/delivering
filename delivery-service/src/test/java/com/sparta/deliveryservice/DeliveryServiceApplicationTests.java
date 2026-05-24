2ca1d4be49bdb6c64ccb2c403f8836c622cfeebc
rlaalsdn0421 <rlaalsdn0421@naver.com>
2026-05-24 23:55:16 +0900

    fix : [KMW] DeliveryRouteStatus.java 아크유닛 통과를 위해 이름 변경

--- delivery-service/src/test/java/com/sparta/deliveryservice/DeliveryServiceApplicationTests.java
+++ delivery-service/src/test/java/com/sparta/deliveryservice/DeliveryServiceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.sparta.common.architecture.BaseArchitectureTest.domain_prefix_naming_rule;
 
-@AnalyzeClasses(packages = "com.sparta.deliveryService",
+@AnalyzeClasses(packages = "com.sparta.deliveryservice",
         importOptions = {
                 ImportOption.DoNotIncludeJars.class,  // 외부 라이브러리 스캔 제외 (성능 최적화 핵심)
                 ImportOption.DoNotIncludeTests.class  // 테스트 코드끼리 검사하는 것 제외
