1d704f893ad4513ff15d221bc9a42bec83116948
rodongwan <rodongwan@burritowallet.com>
2026-05-24 20:23:17 +0900

    feat: dto 추가

---
+++ order-service/src/main/java/com/sparta/orderservice/order/infrastructure/feign/dto/ProductOptionDetailsRequest.java
@@ -0,0 +1,17 @@
+package com.sparta.orderservice.order.infrastructure.feign.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductOptionDetailsRequest {
+    private List<UUID> productOptionIds;
+}
