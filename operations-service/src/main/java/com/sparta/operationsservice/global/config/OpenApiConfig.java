a81090e4279a0d8cc2d2bac60d733765ff4c6d27
rodongwan <rodongwan@burritowallet.com>
2026-05-24 14:40:09 +0900

    fix: openapi config 추가

--- operations-service/src/main/java/com/sparta/operationsservice/global/config/OpenApiConfig.java
+++ operations-service/src/main/java/com/sparta/operationsservice/global/config/OpenApiConfig.java
@@ -2,18 +2,22 @@ package com.sparta.operationsservice.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI operationsServiceOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Operations Service API")
-                        .description("Operations Service API Documentation")
-                        .version("v1.0"));
+                        .title("Operations Service API 명세서")
+                        .description("Operations Service API documentation")
+                        .version("v1.0.0"))
+                .servers(List.of(new Server().url("/operations").description("Default Server URL through Gateway")));
     }
 }
