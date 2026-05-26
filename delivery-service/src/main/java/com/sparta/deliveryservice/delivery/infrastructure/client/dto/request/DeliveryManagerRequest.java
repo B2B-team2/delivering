15f3dcf08507a2c43b382637a929f859c3c018c6
rlaalsdn0421 <rlaalsdn0421@naver.com>
2026-05-26 15:42:31 +0900

    fix : [KMW] 유저로 부터 받아와야할 요청

---
+++ delivery-service/src/main/java/com/sparta/deliveryservice/delivery/infrastructure/client/dto/request/DeliveryManagerRequest.java
@@ -0,0 +1,16 @@
+package com.sparta.deliveryservice.delivery.infrastructure.client.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliveryManagerRequest {
+    private UUID fromhubId;
+}
