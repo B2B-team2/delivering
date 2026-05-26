1f0f4f3b71fc32637a6ca73a2d73d31daf7c01ca
rlaalsdn0421 <rlaalsdn0421@naver.com>
2026-05-26 16:26:04 +0900

    fix : delivery 오픈 페인 양식 통일화

--- delivery-service/src/main/java/com/sparta/deliveryservice/delivery/infrastructure/client/dto/response/DeliveryOrderCancelResponse.java
+++ delivery-service/src/main/java/com/sparta/deliveryservice/delivery/infrastructure/client/dto/response/DeliveryOrderCancelResponse.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeliveryOrderCancelResponse {
-    private UUID orderId;
     private int cancelledCount;
     private int skippedCount;
     private List<CancelledDeliveryDto> cancelledDeliveries;
