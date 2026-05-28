f4b350b654e5d3c09f69062b8ed4f4dca977d67d
rlaalsdn0421 <rlaalsdn0421@naver.com>
2026-05-28 16:23:38 +0900

    fix : 딜리버리 컨트롤러 수정

--- delivery-service/src/main/java/com/sparta/deliveryservice/delivery/presentation/controller/DeliveryController.java
+++ delivery-service/src/main/java/com/sparta/deliveryservice/delivery/presentation/controller/DeliveryController.java
@@ -93,14 +93,14 @@ public class DeliveryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @PatchMapping("/start/{trackingNumber}")
+    @PatchMapping("/deliveries/start/{trackingNumber}")
     public ResponseEntity<ApiResponse<DeliveryStatusResponse>> startDelivery(
             @PathVariable("trackingNumber") String trackingNumber, @RequestHeader("X-User-Id")  UUID userId) {
         DeliveryStatusResponse response = deliveryService.startDelivery(trackingNumber, userId);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    @PatchMapping("/complete/{trackingNumber}")
+    @PatchMapping("/deliveries/complete/{trackingNumber}")
     public ResponseEntity<ApiResponse<DeliveryStatusResponse>> completeDelivery(
             @PathVariable("trackingNumber") String trackingNumber, @RequestHeader("X-User-Id")  UUID userId) {
         DeliveryStatusResponse response = deliveryService.completeDelivery(trackingNumber, userId);
