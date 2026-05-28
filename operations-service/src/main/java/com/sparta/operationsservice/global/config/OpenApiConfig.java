a0176e04d4e7981e13c1dcee6647c7df1bdc4784
won2dev-backand <seodongwoncp@gmail.com>
2026-05-28 15:27:35 +0900

    feat: 전 서비스 Swagger Bearer 인증 추가

--- operations-service/src/main/java/com/sparta/operationsservice/global/config/OpenApiConfig.java
+++ operations-service/src/main/java/com/sparta/operationsservice/global/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.sparta.operationsservice.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +16,24 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI operationsServiceOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Operations Service API 명세서")
                         .description("Operations Service API documentation")
                         .version("v1.0.0"))
-                .servers(List.of(new Server().url("/operations").description("Default Server URL through Gateway")));
+                .servers(List.of(new Server().url("/operations").description("Default Server URL through Gateway")))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
     }
 }
--- user-service/src/main/java/com/sparta/userservice/global/config/SwaggerConfig.java
+++ user-service/src/main/java/com/sparta/userservice/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.sparta.userservice.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,23 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("User Service API")
                         .description("스파르타 물류 - 사용자 및 배송담당자 관리 API")
-                        .version("v1.0"));
+                        .version("v1.0"))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
     }
 }
